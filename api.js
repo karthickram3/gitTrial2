5faf52261ccd1339e851c891878ca533ca0cd373
karthickram3 <43877504+karthickram3@users.noreply.github.com>
2018-12-13 17:18:24 +0530

    changes

---
+++ api.js
@@ -0,0 +1,9 @@
+exports.getmethod = function (req, res) {
+    res.send("HI EVERYBODY")
+}
+exports.getmethod2 = function (req, res) {
+    res.send("HI afadsfas")
+}
+exports.getmethod3 = function (req, res) {
+    res.send("HEY dfasdfsdaf")
+}
--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 const express = require("express")
 const app = express()
 const bodyParser = require('body-parser')
+const api = require("./api")
+
+
+
 
 app.use(bodyParser.json())
+
 
 app.listen(8000, function () {
     console.log("server listening at 8000")
 
 })
 
-app.get("/get", function (req, res) {
-    res.send("HI EVERYBODY")
-})
 
-app.get("/rr", function (req, res) {
-    res.send("HHHHHHHHHHHHH")
-})
-app.get("/rr222", function (req, res) {
-    res.send("dfaHHdfdHHHHHsfdgHHHHHH")
-})
 
-app.get("/llll", function (req, res) {
-    res.send("HHasfsdfsdsdfHHHHHHHHHH")
-})
+app.get("/get", api.getmethod)
+
+app.get("/rr", api.getmethod2)
+
+
+app.get("/llll", api.getmethod3)
