87ccdfdc8966d2a4719f8b0cdce4c1f5f9d5ad9c
karthickram3 <43877504+karthickram3@users.noreply.github.com>
2018-12-13 14:01:09 +0530

    branch commit

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 const express = require("express")
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+app.listen(8000, function () {
+    console.log("server listening at 8000")
+
+})
+
+app.get("/get", function (req, res) {
+    res.send("HI EVERYBODY")
+})
